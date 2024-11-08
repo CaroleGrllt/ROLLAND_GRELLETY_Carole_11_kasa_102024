8fa83fae4edc0a9af0e00a15e62ad02856c7d1c4
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2024-11-08 11:07:58 +0100

    Corrections path react-scripts

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; //utiliser ReactDOM pour s'attacher à notre HTML.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //préciser où notre app React va vivre dans notre HTML
 root.render(
   <React.StrictMode>
   </React.StrictMode>
